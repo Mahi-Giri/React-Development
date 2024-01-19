73d3561bc92591a116f62d28db2551da0be5a882
Mahi <maheshgiri9561@gmail.com>
2024-01-20 00:44:53 +0530

    Learning Component

--- First React Code/app.js
+++ First React Code/app.js
@@ -1,17 +1,40 @@
-import React from "react"; 
+import React from "react";
 import ReactDOM from "react-dom";
-
-const nestedDom = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child1" }, [
-        React.createElement("h1", {}, "I am h1 heading"),
-        React.createElement("h2", {}, "I am h2 heading"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I am h1 heading"),
-        React.createElement("h2", {}, "I am h2 heading"),
-    ]),
-]);
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-root.render(nestedDom);
+// JSX
+const jsxHeading = (
+  <h1 id="heading" className="heading">
+    Hello React! from JSX
+  </h1>
+);
+
+// React Component
+const title = <h1 id="title">Element Component in JSX</h1>;
+
+const Title = () => {
+  return <h1 id="heading">Functional Component in JSX</h1>;
+};
+
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      {/* accessing using Element Component*/}
+      {title}
+
+      {/* accessing using functional Component */}
+      {/* Both are doing same work you can call as function or excuting using functional component */}
+      
+      {<Title />}
+      {Title()}
+      <h2 className="heading">React Functional Component</h2>
+    </div>
+  );
+};
+
+// For rendering React Element
+// root.render(jsxHeading);
+
+// For rendering React Component
+root.render(<HeadingComponent />);
